f4029aab95aff137444fe34eece70c125fc8429d
mandemeskel <mandemeskel@users.noreply.github.com>
2018-03-08 00:49:51 -0800

    adds method declerations

--- execute.h
+++ execute.h
@@ -6,5 +6,25 @@
 #endif
 
 int Execute (DecodedInstr*, RegVals*);
+int getAluOperation (DecodedInstr*, RegVals*);
+
+int getParam1 (int (int, int), DecodedInstr*);
+int getRFormatParam1 (int (int, int), DecodedInstr*);
+int getIFormatParam1 (int (int, int), DecodedInstr*);
+
+int getParam2 (int (int, int), DecodedInstr*);
+int getRFormatParam2 (int (int, int), DecodedInstr*);
+int getIFormatParam2 (int (int, int), DecodedInstr*);
+
+int executeOperation (int (int, int), int, int);
+
+int addOp (int, int);
+int subOp (int, int);
+int sllOp (int, int);
+int srlOp (int, int);
+int andOp (int, int);
+int orOp (int, int);
+int sltbOp (int, int);
+int luiOp (int, int);
 
 #endif
