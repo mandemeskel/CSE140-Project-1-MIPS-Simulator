bb8f493e4610b251c29e22c69fc6236212fbab87
mandemeskel <mandemeskel@users.noreply.github.com>
2018-03-07 19:40:25 -0800

    implements Add test for execute

--- testExecute.c
+++ testExecute.c
@@ -4,20 +4,38 @@
 #include "helper.h"
 #include "computer.h"
 
-#define TRUE 1
-#define FALSE 0
-
-void testExecute();
+void testExecuteAdd();
 
 int main (int argc, char *argv[]) {
     printLine("Running execute test suite...");
 
-    testExecute();
+    testExecuteAdd();
 
     printLine("All execute tests passed!");
 }
 
-void testExecute() {
+void testExecuteAdd() {
+    printf("test add $t8, $t9, $10 - 4 + 5...");
+    DecodedInstr dInst = {
+        .type = R,
+        .op = R_FORMAT_OPCODE,
+        .regs.r.rs = 12,
+        .regs.r.rt = 13,
+        .regs.r.rd = 11,
+        .regs.r.shamt = 0,
+        .regs.r.funct = ADDU_FUNCT
+    };
+    RegVals regVals;
+    int param1 = 4, param2 = 5;
+    int expectedVal = param1 + param2;
 
+    mips.registers[dInst.regs.r.rs] = param1;
+    mips.registers[dInst.regs.r.rt] = param2;
+    mips.registers[dInst.regs.r.rd] = 0;
+
+    int val = Execute(&dInst, &regVals);
+    assertTrue(expectedVal, val, "val does not match expected value");
+
+    printLine("passed");
 }
 
