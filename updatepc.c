1a0582aa3a2324ecd14977a56252deca12427fe1
mandemeskel <mandemeskel@users.noreply.github.com>
2018-03-08 14:33:26 -0800

    implements UpdatePC
    does validate the instruction address - idk if we have to

--- updatepc.c
+++ updatepc.c
@@ -4,6 +4,9 @@
 #include "helper.h"
 #include "updatepc.h"
 
+// register ra's index in mips.registers
+int const R_RA = 31;
+
 /* 
  * Update the program counter based on the current instruction. For
  * instructions other than branches and jumps, for example, the PC
@@ -11,5 +14,32 @@
  */
 void UpdatePC ( DecodedInstr* d, int val) {
     mips.pc+=4;
-    /* Your code goes here */
+    
+    if (d->op == BEQ_OPCODE) {
+
+        if(val == 0) mips.pc = d->regs.i.addr_or_immed;
+        return;
+
+    } else if (d->op == BNE_OPCODE) {
+
+        if(val != 0) mips.pc = d->regs.i.addr_or_immed;
+        return;
+
+    } else if (d->op == JAL_OPCODE) {
+
+        mips.registers[R_RA] = mips.pc; 
+        mips.pc = d->regs.j.target;
+        return;
+
+    } else if (d->op == J_OPCODE) {
+
+        mips.pc = d->regs.j.target;
+        return;
+
+    } else if (d->op == R_FORMAT_OPCODE && d->regs.r.funct == JR_FUNCT) {
+
+        mips.pc = mips.registers[d->regs.r.rs];
+        return;
+
+    }
 }
