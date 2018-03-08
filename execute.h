2ea73926a97a5285e406dc782ea4c4968d267d5d
mandemeskel <mandemeskel@users.noreply.github.com>
2018-03-08 02:09:43 -0800

    function pointers are a pain in the neck

--- execute.h
+++ execute.h
@@ -9,15 +9,15 @@ int Execute (DecodedInstr*, RegVals*);
 int getAluOperation (DecodedInstr*);
 int functionCodeToAluOp (int);
 
-int getParam1 (int (int, int), DecodedInstr*);
-int getRFormatParam1 (int (int, int), DecodedInstr*);
-int getIFormatParam1 (int (int, int), DecodedInstr*);
+int getParam1 (int, DecodedInstr*);
+int getRFormatParam1 (int, DecodedInstr*);
+int getIFormatParam1 (int, DecodedInstr*);
 
-int getParam2 (int (int, int), DecodedInstr*);
-int getRFormatParam2 (int (int, int), DecodedInstr*);
-int getIFormatParam2 (int (int, int), DecodedInstr*);
+int getParam2 (int, DecodedInstr*);
+int getRFormatParam2 (int, DecodedInstr*);
+int getIFormatParam2 (int, DecodedInstr*);
 
-int executeOperation (int (int, int), int, int);
+int executeOperation (int, int, int);
 
 int addOp (int, int);
 int subOp (int, int);
