6d5590ab2ccd71a9ab3a3d74e02f46154bd1e867
mandemeskel <mandemeskel@users.noreply.github.com>
2018-03-08 00:51:43 -0800

    implements ALU operations

--- execute.c
+++ execute.c
@@ -10,3 +10,44 @@ int Execute ( DecodedInstr* d, RegVals* rVals) {
     /* Your code goes here */
   return 0;
 }
+
+/* ALU operation for Execute, param1 + param2. */
+int addOp (int param1, int param2) {
+  return param1 + param2;
+}
+
+/* ALU operation for Execute, subs, param1 - param2. */
+int subOp (int param1, int param2) {
+  return param1 - param2;
+}
+
+/* ALU operation for Execute, param1 << param2. */
+int sllOp (int param1, int param2) {
+  return param1 << param2;
+}
+
+/* ALU operation for Execute, param1 >> param2. */
+int srlOp (int param1, int param2) {
+  return param1 >> param2;
+}
+
+/* ALU operation for Execute, param1 & param2. */
+int andOp (int param1, int param2) {
+  return param1 & param2;
+}
+
+/* ALU operation for Execute, param1 | param2. */
+int orOp (int param1, int param2) {
+  return param1 | param2;
+}
+
+/* ALU operation for Execute, param1 < param2: 1 else 0. */
+int sltbOp (int param1, int param2) {
+  return param1 < param2;
+}
+
+/* ALU operation for Execute, param1 << 16. */
+int luiOp (int param1, int param2) {
+  return param1 << 16;
+}
+
